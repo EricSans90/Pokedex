1be2c01e053d9b3746e366d1cc6055967af98d70
Eric_Sans <esansvi@gmail.com>
2024-01-23 05:25:33 +0100

    Versión aceptable de la PokemonDetailView de los pokémons con el color de los Chips automáticos en función del tipo y sale 1 u dos en funcion de los tipos que tengan

--- app/src/main/java/com/example/pokedex/ui/components/PokemonDetailView.kt
+++ app/src/main/java/com/example/pokedex/ui/components/PokemonDetailView.kt
@@ -2,58 +2,240 @@ package com.example.pokedex.ui.components
 
 //Para mostrar el detalle de un Pokémon
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.AlertDialogDefaults.containerColor
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.toLowerCase
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.rememberImagePainter
 import com.example.pokedex.R
 import com.example.pokedex.domain.models.Pokemon
+import com.example.pokedex.domain.models.PokemonType
+import com.example.pokedex.ui.theme.PokemonTypeColors
+import com.example.pokedex.ui.theme.StatATK
+import com.example.pokedex.ui.theme.StatDEF
+import com.example.pokedex.ui.theme.StatHP
+import com.example.pokedex.ui.theme.StatSATK
+import com.example.pokedex.ui.theme.StatSDEF
+import com.example.pokedex.ui.theme.StatSPD
+import com.example.pokedex.ui.theme.backDetail
+import com.example.pokedex.ui.theme.backGr
+import java.util.Locale
 
 @Composable
 fun PokemonDetailView(pokemon: Pokemon) {
     // UI para mostrar el detalle de un Pokemon
-    Column(){
-        //Mostramos primer sprite disponible
-        ImageLoader(
-            imageUrl = pokemon.sprites.frontDefault,
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .background(color = backGr)
+        //.padding(12.dp)
+        ,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+
+        Card(
+            elevation = CardDefaults.cardElevation(defaultElevation = 6.dp),
+            shape = RoundedCornerShape(topStart = 0.dp, topEnd = 0.dp, bottomStart = 36.dp, bottomEnd = 36.dp),
             modifier = Modifier
-                .fillMaxWidth()
-                .height(180.dp)
-                .clip(RoundedCornerShape(12.dp)),
-            contentScale = ContentScale.Crop
-        )
+                .fillMaxWidth(),
+            colors = CardDefaults.cardColors(containerColor = backDetail)
+        ) {
+
+            Column() {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                ) {
+                    Row() {
+                        IconButton(onClick = {/* navegacion al menú */ }) {
+                            Icon(
+                                Icons.Default.ArrowBack,
+                                contentDescription = null,
+                                tint = Color.White
+                            )
+                        }
+                        Text(
+                            text = "Pokedex",
+                            color = Color.White,
+                            modifier = Modifier.padding(top = 10.dp)
+
+                        )
+                    }
+                    Text(
+                        text = "#${pokemon.id}",
+                        color = Color.White,
+                        modifier = Modifier.padding(top = 10.dp, end = 10.dp)
+
+                    )
+                }
+
+                ImageLoader(
+                    // SUpongo que frontDefault está siempre
+                    // y sino pongo una imagen por defecto en el placeholder
+                    imageUrl = pokemon.sprites.frontDefault,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(180.dp)
+                        .clip(RoundedCornerShape(12.dp)),
+                    contentScale = ContentScale.Crop
+                )
+            }
+        }
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Row(modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,){
+            Text(text = "${pokemon.name}",
+                color = Color.White,
+                fontFamily = FontFamily(Font(R.font.aliviaregular)),
+                fontSize = 45.sp
+            )
+        }
+        Spacer(modifier = Modifier.height(0.dp))
+        if(pokemon.types.size ==1){
+            TypeChips(types = listOf(pokemon.types.first()))
+        } else if (pokemon.types.size > 1){
+            TypeChips(types = pokemon.types)
+        }
+
+        //Text(text = "Types: ${pokemon.types.joinToString { it.type.name }}", color = Color.White)
         Spacer(modifier = Modifier.height(8.dp))
-        Text(text = "Name: ${pokemon.name}")
-        Spacer(modifier = Modifier.height(8.dp))
-        Text(text = "Types: ${pokemon.types.joinToString { it.type.name }}")
-        Spacer(modifier = Modifier.height(8.dp))
-        Text(text = "Weight: ${pokemon.weight}")
-        Spacer(modifier = Modifier.height(8.dp))
-        Text(text = "Height: ${pokemon.height}")
-        Spacer(modifier = Modifier.height(8.dp))
-        Text(text = "Base Stats:")
-        LazyRow {
-            items(pokemon.stats) { stat ->
-                Text(text = "${stat.stat.name}: ${stat.baseStat}")
+        Row(modifier = Modifier.fillMaxWidth()
+            .padding(horizontal = 16.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            ){
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally){
+                Text(text = "${pokemon.weight} KG",
+                    color = Color.White,
+                    fontSize = 30.sp)
+                Text(text = "Weight",
+                    color = Color.Gray)
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Column(){
+                Text(text = "${pokemon.height} M",
+                    color = Color.White,
+                    fontSize = 30.sp)
+                Text(text = "Height",
+                    color = Color.Gray)
             }
         }
 
-    }
+        Spacer(modifier = Modifier.height(24.dp))
+        Text(text = "Base Stats",
+            color = Color.White,
+            fontSize = 25.sp)
+        Column (verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            pokemon.stats.forEach { stat ->
+                Text(text = "${getStatName(stat.stat.name)}: ${stat.baseStat}",
+                    color = Color.White)
 
+                //Spacer(modifier = Modifier.height(8.dp))
+                //Aunque en el pokemon cada pokemon tiene unos stats máximos diferentes,
+                // nosotros trabajaremos con 255 de número teórico, pero dependen de cada
+                // pokemon individual, no todos pueden llegar a 255
+                val MAX_STAT_VALUE = 255
+                LinearProgressIndicator(progress = stat.baseStat.toFloat() / MAX_STAT_VALUE,
+                    modifier=Modifier.heightIn(12.dp)
+                        .clip(RoundedCornerShape(50)),
+                    color = getStatColor(stat.stat.name)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun ChipView(typeName: String) {
+    val chipColor = PokemonTypeColors[typeName.toLowerCase()] ?: Color.Gray
+    Card(
+        elevation = CardDefaults.cardElevation(defaultElevation = 6.dp),
+        shape = RoundedCornerShape(50),
+        modifier = Modifier
+            .padding(4.dp)
+            .clip(RoundedCornerShape(50)),
+        colors = CardDefaults.cardColors(containerColor = chipColor)
+    ) {
+        Text(
+            text = typeName,
+            modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
+            color = Color.White
+        )
+    }
+}
+
+
+@Composable
+fun TypeChips(types: List<PokemonType>) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        types.forEach { type ->
+            ChipView(typeName = type.type.name)
+        }
+    }
+}
+
+fun getStatColor(statName: String): Color{
+    return when (statName){
+        "hp" -> StatHP
+        "attack" -> StatATK
+        "defense" -> StatDEF
+        "special-attack" -> StatSATK
+        "special-defense" -> StatSDEF
+        "speed" -> StatSPD
+        else -> Color.Gray
+    }
+}
+
+@Composable
+fun getStatName(statName: String): String{
+    return when (statName){
+        "hp" -> "HP"
+        "attack" -> "ATK"
+        "defense" -> "DEF"
+        "special-attack" -> "S-ATK"
+        "special-defense" -> "S-DEF"
+        "speed" -> "SPD"
+        else -> "Stat"
+    }
 }
 
 @Composable
--- app/src/main/java/com/example/pokedex/ui/theme/Color.kt
+++ app/src/main/java/com/example/pokedex/ui/theme/Color.kt
@@ -9,3 +9,52 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val backDetail = Color(0xFFF77A53)
+val backGr = Color(0xFF242222)
+
+val StatHP = Color(0xFF4CAF50)
+val StatATK = Color(0xFFF44336)
+val StatDEF = Color(0xFF2196F3)
+val StatSATK = Color(0xFF9C27B0)
+val StatSDEF = Color(0xFF3F51B5)
+val StatSPD = Color(0xFFFFC107)
+
+val normal = Color.Gray
+val fire = Color(0xFFD51E11)
+val water = Color(0xFF539DDF)
+val grass = Color(0xFF3C9B35)
+val flying = Color(0xFFA1BBEC)
+val fighting = Color(0xFFD3425F)
+val poison = Color(0xFFB763CF)
+val electric = Color(0xFFF2D94E)
+val ground = Color(0xFFDA7C4D)
+val rock = Color(0xFF582413)
+val psychic = Color(0xFFFF6675)
+val bug = Color(0xFF92BC2C)
+val ghost = Color(0xFF6610FF)
+val steel = Color(0xFF5695A3)
+val dragon = Color(0xFF0C69C8)
+val dark = Color(0xFF595761)
+val fairy =Color(0xFFEE90E6)
+
+val PokemonTypeColors = mapOf(
+    "normal" to Color.Gray,
+    "fire" to Color(0xFFD51E11),
+    "water" to Color(0xFF539DDF),
+    "grass" to Color(0xFF3C9B35),
+    "flying" to Color(0xFFA1BBEC),
+    "fighting" to Color(0xFFD3425F),
+    "poison" to Color(0xFFB763CF),
+    "electric" to Color(0xFFF2D94E),
+    "ground" to Color(0xFFDA7C4D),
+    "rock" to Color(0xFF582413),
+    "psychic" to Color(0xFFFF6675),
+    "ice" to Color(0xFF75D0C1),
+    "bug" to Color(0xFF92BC2C),
+    "ghost" to Color(0xFF6610FF),
+    "steel" to Color(0xFF5695A3),
+    "dragon" to Color(0xFF0C69C8),
+    "dark" to Color(0xFF595761),
+    "fairy" to Color(0xFFEE90E6)
+)
